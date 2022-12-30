74db292eb448250b1f0b1726ab3bd2e057da4e62
Vika <vikka-94@mail.ru>
2022-12-30 22:27:15 +0300

    dz

---
+++ 52/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = CreateRandom2DArray(3, 4);
+
+Print2DArray(array);
+
+double[] newarray = FindColumnsElementsAverage(array);
+
+PrintArray(newarray);
+
+
+int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+{
+    Random random = new Random();
+    int[,] array = new int[countOfRows, countOfColumns];
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 11);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();   
+    }
+}
+
+double[] FindColumnsElementsAverage(int[,] array)
+{
+
+    double[] newarray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+            sum = sum + array[i, j];
+        newarray[j] = Math.Round((sum / array.GetLength(0)),1);
+    }  
+    return newarray;
+}
+
+void PrintArray(double[] array)
+{
+        for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
